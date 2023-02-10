128fcb4b207f7042514fb426a8f824cf239547b8
khadri2021 <khadri.institutions@gmail.com>
2023-02-10 19:38:30 +0530

    commits

--- src/com/khadri/modern/functional/Test.java
+++ src/com/khadri/modern/functional/Test.java
@@ -3,6 +3,7 @@ package com.khadri.modern.functional;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("added");
 		FunctionalInterf1 interf1 = () -> {
 			System.out.println("Hello Functional Interface Implementation");
 		};
